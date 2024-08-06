c0b3b04d8fe21f6556958b6d316a6e1c04cc3db6
Bruno <brunolmf@ufmg.br>
2024-08-06 11:30:42 -0300

    Adição do teste de jogadas possiveis na diagonal do reversi

--- src/Reversi.cpp
+++ src/Reversi.cpp
@@ -40,7 +40,8 @@ bool Reversi::verifyPlay(int row, int col, char symbol) {
     char opposite = (symbol == 'X') ? 'O' : 'X';
 
     // Verificando se a jogada é válida verificando as casas ao redor 
-    if (getSquare(row-1, col) == opposite || getSquare(row+1, col) == opposite) // VERIFICA SE HÁ SIMBOLO OPOSTO NAS LATERAIS
+    if (getSquare(row-1, col) == opposite || getSquare(row+1, col) == opposite) // VERIFICA SE HÁ SIMBOLO OPOSTO NAS LATERAIS  
+    //POSSIVEL ACCESO INVALIDO? - BRUNO
     {
         // VERIFICAR SE DEPOIS DO SÍMBOLO OPOSTO HÁ UM DO MESMO SÍMBOLO NA MESMA LINHA
         for (int i = row -1; i > 0; i--)
@@ -63,6 +64,7 @@ bool Reversi::verifyPlay(int row, int col, char symbol) {
     }
 
     if (getSquare(row, col-1) == opposite || getSquare(row, col+1) == opposite) // VERIFICA SE HÁ SIMBOLO OPOSTO NAS EM CIMA OU EM BAIXO
+    //POSSIVEL ACESSO INVALIDO EM -1? - BRUNO
     {
         // VERIFICAR SE DEPOIS DO SÍMBOLO OPOSTO HÁ UM DO MESMO SÍMBOLO NA MESMA COLUNA
         for (int i = col -1; i > 0; i--)
@@ -83,7 +85,43 @@ bool Reversi::verifyPlay(int row, int col, char symbol) {
         }
 
     }
-
+    if (getSquare(row-1, col-1) == opposite || getSquare(row+1, col+1) == opposite) // VERIFICA SE HÁ SIMBOLO OPOSTO NA DIAGONAL
+    {
+        // VERIFICAR SE DEPOIS DO SÍMBOLO OPOSTO HÁ UM DO MESMO SÍMBOLO NA MESMA DIAGONAL
+        for (int i = row -1, j = col -1; i > 0 && j > 0; i--, j--) //VERIFICA A ESQUERDA E CIMA
+        {
+            if (getSquare(i, j)== symbol)
+            {
+                valid = true; 
+                reverseSymbols(i, j);
+            }
+        }
+        for (int i = row + 1, j = col + 1; i < 8 && j < 8; i++, j++) //VERIFICA A DIREITA E BAIXO
+        {
+            if (getSquare(i, j)== symbol)
+            {
+                valid = true; 
+                reverseSymbols(i, j);
+            }
+        }
+        // VERIFICA A DIREITA PRA CIMA  
+        for (int i = row - 1, j = col + 1; i > 0 && j < 8; i--, j++)
+        {
+            if (getSquare(i, j)== symbol)
+            {
+                valid = true; 
+                reverseSymbols(i, j);
+            }
+        }
+        for (int i = row + 1, j = col - 1; i < 8 && j > 0; i++, j--) //VERIFICA A ESQUERDA E BAIXO
+        {
+            if (getSquare(i, j)== symbol)
+            {
+                valid = true; 
+                reverseSymbols(i, j);
+            }
+        }
+    }
     return valid;
 }
 
