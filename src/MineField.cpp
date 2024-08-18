c96b047f18d0f1e03b4d7eb4e2ad798eba6a8626
Bruno <brunolmf@ufmg.br>
2024-08-18 19:17:53 -0300

    funçoes de status

--- src/MineField.cpp
+++ src/MineField.cpp
@@ -165,6 +165,7 @@ void MineField::play(){
             try{
                 if(!makePlay(move)){
                     std::cout << getCurrentPlayer()->getNickname() <<" perdeu!" << std::endl;
+                    player -> loseMineField();
                     break;
                 }
                 if(isGameOver()){
@@ -188,7 +189,7 @@ void MineField::play(){
             //todo addstatus
         }else{
             std::cout << "Vitória de " << (p1win ? getPlayer1()->getNickname() : getPlayer2()->getNickname()) << std::endl;
-            //todo addstatus
+            player -> winMineField();
         }
     }
 }
--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -72,8 +72,8 @@ void TicTacToe::addStats(Player* winner, Player* loser){
         std::cout << "Partidas contra o computador não contam para as estatísticas." << std::endl;
         return;
     }
-    //winner->increaseVictories();
-    //loser->increaseDefeats();
+    winner->winTicTacToe();
+    loser->loseTicTacToe();
 }
 
 bool TicTacToe::verifySequence(const BoardType &board){
